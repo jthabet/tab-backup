9c5eccda509e7ea076cc8f5df847d419f5cfc51e
jthabet <32544469+jthabet@users.noreply.github.com>
2023-03-23 02:06:58 +0100

    Added react and move into component.

--- src/popup.js
+++ src/popup.js
@@ -1,50 +1,135 @@
+import { StrictMode, useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import dayjs from "dayjs";
+
 import "./main.css";
 
-function listTabs() {
-  getCurrentWindowTabs().then((tabs) => {
-    const tabsList = document.getElementById("tabs-list");
-    const currentTabs = document.createDocumentFragment();
-    const limit = 25;
-    let counter = 0;
+function Popup() {
+  return (
+    <div className="relative m-1">
+      <NavBar />
 
-    tabsList.textContent = "";
-
-    for (let tab of tabs) {
-      if (counter <= limit) {
-        const li = document.createElement("li");
-
-        li.textContent = tab.title || tab.id;
-        li.classList.add("block");
-        li.classList.add("dark:text-white");
-
-        currentTabs.appendChild(li);
-      }
-
-      counter += 1;
-    }
-
-    tabsList.appendChild(currentTabs);
-  });
+      <div className="mt-3 border-t border-slate-700 px-3 ">
+        <TabsList />
+      </div>
+    </div>
+  );
 }
 
-document.addEventListener("DOMContentLoaded", listTabs);
+function NavBar() {
+  return (
+    <div className="sticky top-0  flex h-2/5 justify-center space-x-3 pt-2 dark:bg-slate-900">
+      <button
+        className="rounded-full p-2.5 text-center hover:bg-slate-700"
+        onClick={() => exportCurrentTabs()}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-device-floppy pointer-events-none"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2"></path>
+          <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+          <path d="M14 4l0 4l-6 0l0 -4"></path>
+        </svg>
+      </button>
+
+      <button
+        className="rounded-full p-2.5 text-center hover:bg-slate-700"
+        onClick={() => {
+          browser.runtime.openOptionsPage();
+          window.close();
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-file-arrow-left pointer-events-none"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+          <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+          <path d="M15 15h-6"></path>
+          <path d="M11.5 17.5l-2.5 -2.5l2.5 -2.5"></path>
+        </svg>
+      </button>
+
+      <button
+        className="rounded-full p-2.5 text-center hover:bg-slate-700"
+        onClick={() => {
+          browser.runtime.openOptionsPage();
+          window.close();
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-settings pointer-events-none"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
+          <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+function TabsList() {
+  const [tabs, setTabs] = useState([]);
+
+  useEffect(() => {
+    getCurrentWindowTabs().then((tabs) => {
+      setTabs(tabs);
+    });
+  }, []);
+
+  return (
+    <ol
+      className="m-1 mt-5 mb-4 w-11/12 list-inside list-disc space-y-3 truncate italic subpixel-antialiased"
+      id="tabs-list"
+    >
+      {tabs.map((tab) => (
+        <a
+          className="hover:not-italic hover:text-blue-400"
+          key={tab.id}
+          href="#"
+          onClick={(e) => {
+            e.preventDefault;
+            browser.tabs.update(tab.id, { active: true });
+          }}
+        >
+          <li>{tab.title}</li>
+        </a>
+      ))}
+    </ol>
+  );
+}
 
 function getCurrentWindowTabs() {
   return browser.tabs.query({ currentWindow: true });
-}
-
-function printCurrentTabs() {
-  const list = new Array();
-
-  getCurrentWindowTabs()
-    .then((tabs) => {
-      for (let tab of tabs) {
-        list.push({ title: tab.title, url: tab.url });
-      }
-    })
-    .finally(() => {
-      console.info(JSON.stringify(list, null, 2));
-    });
 }
 
 function exportCurrentTabs() {
@@ -77,12 +162,16 @@ function exportCurrentTabs() {
         if (downloadDelta.state && downloadDelta.state.current === "complete") {
           // Revoke the object URL
           URL.revokeObjectURL(objURL);
+          window.close();
         }
       });
 
+      const str1 = "tabs";
+      const str2 = dayjs().format("DD-MM-YYYY_HH-mm");
+
       let downloading = browser.downloads.download({
         url: objURL,
-        filename: "tabs.json",
+        filename: str1.concat("-", str2) + ".json",
       });
 
       downloading
@@ -91,16 +180,9 @@ function exportCurrentTabs() {
     });
 }
 
-document.addEventListener("click", (e) => {
-  if (e.target.id === "tabs-print") {
-    printCurrentTabs();
-  } else if (e.target.id === "tabs-export") {
-    exportCurrentTabs();
-  } else if (e.target.id === "tabs-import") {
-    browser.runtime.openOptionsPage();
-  } else if (e.target.id === "option-menu") {
-    browser.runtime.openOptionsPage();
-  }
-
-  e.preventDefault();
-});
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Popup />
+  </StrictMode>
+);
