3dbb83d18440fd90d6d79ca590c4d442fa082f08
jthabet <32544469+jthabet@users.noreply.github.com>
2023-03-24 01:04:06 +0100

    Update with react.

--- src/options.js
+++ src/options.js
@@ -1,16 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import "./main.css";
 
-const fileInput = document.getElementById("input");
+function ImportAction() {
+  return (
+    <div className="flex items-center space-x-6">
+      <label className="inline-block">Import tabs from</label>
 
-fileInput.addEventListener("change", (event) => {
-  const files = event.target.files;
+      <input
+        className="block w-full text-sm text-slate-500 file:mr-4 file:rounded-full file:border-0 file:bg-sky-500 file:py-2 file:px-4 file:text-sm file:font-semibold file:text-sky-100 hover:file:bg-sky-100 hover:file:text-sky-600"
+        type="file"
+        id="input"
+        accept="application/json"
+        onChange={(event) => {
+          const files = event.target.files;
 
-  const reader = new FileReader();
-  reader.onload = (e) => {
-    openImportedTabs(e.target.result);
-  };
-  reader.readAsText(files[0]);
-});
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            openImportedTabs(e.target.result);
+          };
+          reader.readAsText(files[0]);
+        }}
+      />
+    </div>
+  );
+}
 
 function openImportedTabs(text) {
   let objects;
@@ -40,8 +55,9 @@ function openImportedTabs(text) {
   }
 }
 
-function retrieveCurrentExtStorage() {
-  browser.storage.sync
-    .getBytesInUse()
-    .then((res) => console.log("current storage is " + res));
-}
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <ImportAction />
+  </StrictMode>
+);
